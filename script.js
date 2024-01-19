41cc410fe07674e19aaaf8067490b26d002a66a8
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-01-19 22:38:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let year=prompt("Enter Year")
+
+ function daysOfAYear(year)
+{
+     if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+        console.log("366");
+     } 
+     else {
+    console.log("365")
+  }
+}
+
+daysOfAYear(year);
